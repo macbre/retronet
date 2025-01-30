69a0b91d358fde3b40169af741153922178d4520
macbre <maciej.brencz@gmail.com>
2025-01-30 19:03:09 +0000

    Add the screenshot.mjs file

---
+++ screenshot.mjs
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+import { setTimeout } from 'node:timers/promises';
+import { writeFile } from 'node:fs/promises';
+
+import log from 'npmlog';
+import puppeteer from 'puppeteer';
+
+const url = 'https://web.archive.org/web/20060621110703/http://www.faroejet.fo/';
+
+log.info('Rendering', `<${url}> ...`);
+
+(async () => {
+  const browser = await puppeteer.launch({headless: 'new'});
+  const page = await browser.newPage();
+
+  await page.setViewport({width: 2014 / 2, height: 1434 / 2, deviceScaleFactor: 2});
+
+  page.on('requestfinished', (req) => {
+    log.info('Response', `<${req.url()}>`);
+  });
+
+  page.on('console', msg => log.info('log: ', msg.text()));
+
+  const then = Date.now();
+  await page.goto(url, {waitUntil: 'networkidle0'});
+  const took = Date.now() - then;
+
+  log.info(`Page loaded`, `in ${took} ms`);
+
+  // close the Web Archive nav bar and get the page title
+  const title = await page.evaluate(() => {
+    document.querySelector('#wm-ipp-base').style.display = 'none';
+    return document.title;
+  });
+  await setTimeout(250);
+  await writeFile('web_archive.txt', title);
+
+  log.info(`Taking the screenshot of the page titled "${title}"...`);
+
+  // wait a bit for the page to load, do some animations, etc.
+  await setTimeout(3000);
+
+  const path = 'web_archive.png';
+  await page.screenshot({path});
+
+  log.info(`Screenshot saved in ${path}`);
+
+  await browser.close();
+  log.info('Done');
+})();
