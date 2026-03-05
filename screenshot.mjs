460535fc0694233ca4798a1eba811bfab5aee5c3
macbre <maciej.brencz@gmail.com>
2026-03-05 22:14:05 +0000

    Use a single file to define the URLs to take screenshots of

--- screenshot.mjs
+++ screenshot.mjs
@@ -6,16 +6,8 @@ import log from 'npmlog';
 import puppeteer from 'puppeteer';
 
 import { getFileNameFromUrl } from './utils.mjs';
+import url from './url.mjs';
 
-// const url = 'https://web.archive.org/web/20060621110703/http://www.faroejet.fo/';
-// const url = 'https://web.archive.org/web/20040130040316/http://fo.wikipedia.org/wiki.cgi';
-//const url = 'https://web.archive.org/web/20010723022643/http://www.bonus.fo/';
-//const url = 'https://web.archive.org/web/20061125065658/http://www.reddit.com/?tbnl-session=9316:0E1D16DC6D639E728538B99D69582C29';
-// const url = 'https://web.archive.org/web/20040701020748/http://flickr.com/';
-// const url = 'https://web.archive.org/web/20130807124247/https://github.com/torvalds/linux';
-// const url = 'https://web.archive.org/web/20090607025726/http://vim.wikia.com/wiki/Vim_Tips_Wiki';
-// const url = 'https://web.archive.org/web/20011211204057/http://eurosport.com/home/sport_0_S25.htm';
-const url = 'https://web.archive.org/web/20040324062019/http://wordpress.org/';
 const path = 'screenshots/' + getFileNameFromUrl(url);
 
 log.info('Rendering', `<${url}> ...`);
---
+++ url.mjs
@@ -0,0 +1,15 @@
+// Update URL of the page to make the screenshot for.
+
+// const page = 'https://web.archive.org/web/20060621110703/http://www.faroejet.fo/';
+// const page = 'https://web.archive.org/web/20040130040316/http://fo.wikipedia.org/wiki.cgi';
+// const page = 'https://web.archive.org/web/20181223194317/http://local.fo/';
+// const page = 'https://web.archive.org/web/20061125065658/http://www.reddit.com/?tbnl-session=9316:0E1D16DC6D639E728538B99D69582C29';
+// const page = 'https://web.archive.org/web/20040701020748/http://flickr.com/';
+// const page = 'https://web.archive.org/web/20130807124247/https://github.com/torvalds/linux';
+// const page = 'https://web.archive.org/web/20090607025726/http://vim.wikia.com/wiki/Vim_Tips_Wiki';
+// const page = 'https://web.archive.org/web/20011211204057/http://eurosport.com/home/sport_0_S25.htm';
+// const page = 'https://web.archive.org/web/20040324062019/http://wordpress.org/';
+const page = 'https://web.archive.org/web/20000229050743/http://ask.com/';
+//           ^^^^
+
+export default page;
